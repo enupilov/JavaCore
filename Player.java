f0df5f72767425e8ff8576e9b92d164e54c7a8fd
enupilov <95871669+enupilov@users.noreply.github.com>
2022-09-26 20:44:33 +0600

    Delete Player.java

--- Player.java
+++
@@ -1,9 +0,0 @@
-package lesson1;
-
-public interface Player {
-    String getName();
-    boolean isOnDistance();
-    void run(int distance);
-    void jump(int height);
-    void swim(int distance);
-}
