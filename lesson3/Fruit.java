e45c91b53b9a103173c8a47d1ec5262339f43b7c
enupilov <enupilov@gmail.com>
2022-10-02 12:47:02 +0600

    Практическое задание №3.

---
+++ lesson3/Fruit.java
@@ -0,0 +1,11 @@
+package lesson3;
+
+public class Fruit {
+    private float weight;
+    Fruit(float weight) {
+        this.weight = weight;
+    }
+    public float getWeight() {
+        return weight;
+    }
+}
---
+++ lesson3/Orange.java
@@ -0,0 +1,11 @@
+package lesson3;
+
+public class Orange extends Fruit {
+    Orange() {
+        super(1.5f);
+    }
+    @Override
+    public String toString(){
+        return "апельсины";
+    }
+}
